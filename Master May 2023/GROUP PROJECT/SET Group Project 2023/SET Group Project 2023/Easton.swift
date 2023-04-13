9581ef5875898edf32184eef6ff6ced939688092
Steven Newman <stevennewman@Stevens-MacBook-Pro.local>
2023-04-13 09:22:00 -0600

    Added a file for both tyler and easton

---
+++ Master May 2023/GROUP PROJECT/SET Group Project 2023/SET Group Project 2023/Easton.swift
@@ -0,0 +1,8 @@
+//
+//  Easton.swift
+//  SET Group Project 2023
+//
+//  Created by Steven Newman on 4/13/23.
+//
+
+import Foundation
--- Master May 2023/GROUP PROJECT/SET Group Project 2023/SET Group Project 2023/ViewController.swift
+++ Master May 2023/GROUP PROJECT/SET Group Project 2023/SET Group Project 2023/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    // Stevie
+    
+// Stevie
     
     
     
